11debf5f4db3b2614549a5a21532c3edd37f62a1
Xeyrix1 <110469394+Xeyrix1@users.noreply.github.com>
2022-12-02 15:11:27 +0700

    Update donasi.cjs

--- plugins/donasi.cjs
+++ plugins/donasi.cjs
@@ -1,14 +1,14 @@
 var handler = async m => m.reply(`
 ╭─「 Donasi 」
-│ • TELKOMSEL [08211499086]
-│ • DANA [08211499086]
-│ • OVO [08211499086]
-│ • GOPAY [08211499086]
-│ • SHOPPY PAY [08211499086]
+│ • TELKOMSEL [081280020649]
+│ • DANA [087877155288]
+│ • OVO [081280020649]
+│ • GOPAY [081280020649]
+│ • SHOPPY PAY [081280020649]
 │ • Saweria [https://saweria.co/botstyle]
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/62895368900456
+│ > Ingin donasi? Wa.me/6281280020649
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
